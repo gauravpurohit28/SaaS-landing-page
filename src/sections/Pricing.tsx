4d80d943057b47c5da3e3d7dd94f314b6b6591dc
Gaurav Purohit <grajpurohittt@gmail.com>
2025-07-15 01:17:43 +0530

    testimonial section added

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -56,7 +56,7 @@ const pricingTiers = [
 ];
 
 export const Pricing = () => {
-  return <section className="py-24">
+  return <section className="py-24 bg-white">
     <div className="container">
       <div className="section-heading">
       <h2 className="section-title">Pricing</h2>
@@ -67,7 +67,7 @@ export const Pricing = () => {
       <div className="flex flex-col gap-6 items-center mt-10 lg:flex-row lg:items-end lg:justify-center">
         {pricingTiers.map(({ title, monthlyPrice, buttonText, popular, inverse, features})=>(
         <div className={twMerge(
-          "p-10 border border-[#F1F1F1] rounded-3xl shadow-[0_7px_14px_#EAEAEA] max-w-xs w-full", 
+          "card", 
           inverse === true && "border-black bg-black text-white"
         )}>
 
